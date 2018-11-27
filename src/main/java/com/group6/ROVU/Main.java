5f080536a7276c5614d76e28748abbdbf68dc995
Aron Sandstedt <aron.sandstedt@live.se>
2018-11-27 13:25:29 +0100

    added main files

---
+++ src/main/java/com/group6/ROVU/Main.java
@@ -0,0 +1,9 @@
+package com.group6.ROVU;
+
+public class Main {
+
+    public static void main (String[] strings) {
+
+    }
+
+}
---
+++ src/main/java/com/group6/RobotRover/Main.java
@@ -0,0 +1,9 @@
+package com.group6.RobotRover;
+
+public class Main {
+
+    public static void main (String[] strings) {
+        
+    }
+
+}
