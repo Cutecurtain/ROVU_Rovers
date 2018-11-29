e3212cdeddf5dd26c0bfda54df4cae304d7378ab
Aron Sandstedt <aron.sandstedt@live.se>
2018-11-29 17:12:05 +0100

    add test files

---
+++ src/test/java/com/group6/Server/ROVU/ControllerTest.java
@@ -0,0 +1,7 @@
+package com.group6.Server.ROVU;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+}
---
+++ src/test/java/com/group6/Server/Robot/RobotTest.java
@@ -0,0 +1,7 @@
+package com.group6.Server.Robot;
+
+import static org.junit.Assert.*;
+
+public class RobotTest {
+
+}
