dfb48e9263e16104df2c9db306e71b44a5b8aa3b
Aron Sandstedt <aron.sandstedt@live.se>
2018-11-29 18:00:26 +0100

    create test environment

--- src/main/java/com/group6/Server/Main.java
+++ src/main/java/com/group6/Server/Main.java
@@ -1,6 +1,13 @@
 package com.group6.Server;
 
+import com.group6.Server.Environment.Area.Area;
+import com.group6.Server.Environment.Area.IArea;
+import com.group6.Server.Environment.Area.Rect;
 import com.group6.Server.Environment.Environment;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -11,7 +18,16 @@ public class Main {
     }
 
     public void main() {
+        List<IArea> areas = new ArrayList<IArea>();
+        IArea area1 = new Area(true, 1);
 
+        // Creating four squares next to each other
+        ((Area) area1).addShape(new Rect(new Point(0, 0), new Point(100, 100)));
+        ((Area) area1).addShape(new Rect(new Point(100, 0), new Point(200, 100)));
+        ((Area) area1).addShape(new Rect(new Point(0, 100), new Point(100, 200)));
+        ((Area) area1).addShape(new Rect(new Point(100, 100), new Point(200, 200)));
+
+        assaignment3 = new Environment(areas);
     }
 
     public void procedureA() {
