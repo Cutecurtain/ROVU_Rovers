9edb9a912d712899de5218cde94ce25786c06fac
Aron Sandstedt <aron.sandstedt@live.se>
2018-11-29 21:54:30 +0100

    fixed mistake

--- src/main/java/com/group6/Server/Main.java
+++ src/main/java/com/group6/Server/Main.java
@@ -48,23 +48,23 @@ public class Main {
 
         // Robot 2:  Enter Room 2, then go to Room 3, then exit
         List<Point> mission2 = new ArrayList<Point>();
-        mission1.add(new Point(150, 50));
-        mission1.add(new Point(150, 150));
-        mission1.add(new Point(250, 150));
+        mission2.add(new Point(150, 50));
+        mission2.add(new Point(150, 150));
+        mission2.add(new Point(250, 150));
 
 
         // Robot 3:  Enter Room 3, then go to Room 4, then exit
         List<Point> mission3 = new ArrayList<Point>();
-        mission1.add(new Point(150, 150));
-        mission1.add(new Point(50, 150));
-        mission1.add(new Point(-50, 150));
+        mission3.add(new Point(150, 150));
+        mission3.add(new Point(50, 150));
+        mission3.add(new Point(-50, 150));
 
 
         // Robot 4:  Enter Room 4, then go to Room 1, then exit
         List<Point> mission4 = new ArrayList<Point>();
-        mission1.add(new Point(50, 150));
-        mission1.add(new Point(50, 50));
-        mission1.add(new Point(-50, 50));
+        mission4.add(new Point(50, 150));
+        mission4.add(new Point(50, 50));
+        mission4.add(new Point(-50, 50));
 
         // Instantiate the missions from the mission points
         IMission m1 = new Mission(mission1);
