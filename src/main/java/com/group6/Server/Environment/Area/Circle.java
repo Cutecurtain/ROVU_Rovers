1433a83b1eac58f72cc3e7f8799e2d3bb59e682d
Aron Sandstedt <aron.sandstedt@live.se>
2018-12-03 16:33:40 +0100

    fix rect and completed circle

--- src/main/java/com/group6/Server/Environment/Area/Circle.java
+++ src/main/java/com/group6/Server/Environment/Area/Circle.java
@@ -3,7 +3,21 @@ package com.group6.Server.Environment.Area;
 import java.awt.*;
 
 public class Circle implements IShape{
+
+    private Point point;
+    private float radius;
+
+    public Circle(Point middlePoint, float radius) {
+        this.point = middlePoint;
+        this.radius = radius;
+    }
+
     public boolean isPosIn(Point point) {
-        return false;
+        float dx = (float) Math.abs(this.point.getX() - point.getX());
+        float dy = (float) Math.abs(this.point.getY() - point.getY());
+
+        float distance = (float) Math.sqrt(dx*dx + dy*dy);
+
+        return distance <= radius;
     }
 }
--- src/main/java/com/group6/Server/Environment/Area/Rect.java
+++ src/main/java/com/group6/Server/Environment/Area/Rect.java
@@ -11,7 +11,19 @@ public class Rect implements IShape{
     }
 
     public boolean isPosIn(Point point) {
-        return point.getX() >= point1.getX() && point.getX() <= point2.getX() &&
-                point.getY() >= point1.getY() && point.getY() <= point2.getY();
+        boolean withinX;
+        boolean withinY;
+
+        if (point1.getX() > point2.getX())
+            withinX = point.getX() >= point1.getX() && point.getX() <= point2.getX();
+        else
+            withinX = point.getX() >= point2.getX() && point.getX() <= point1.getX();
+
+        if (point1.getY() > point2.getY())
+            withinY = point.getY() >= point1.getY() && point.getY() <= point2.getY();
+        else
+            withinY = point.getY() >= point2.getY() && point.getY() <= point1.getY();
+
+        return withinX && withinY;
     }
 }
