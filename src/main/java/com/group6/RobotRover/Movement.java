8a67f9021175ae36f6a18704fd407334156db704
lucarl <carl.e.w.lundborg@gmail.com>
2018-12-06 14:57:27 +0100

    added factory pattern for xor features

--- src/main/java/com/group6/RobotRover/Movement.java
+++ src/main/java/com/group6/RobotRover/Movement.java
@@ -1,4 +1,25 @@
 package com.group6.RobotRover;
 
 public class Movement {
+
+    private double speed;
+
+    private int x, y; //for direction
+
+    public Movement(int dirX, int dirY, double speed, String feature) {
+
+        this.x = dirX;
+        this.y = dirY;
+        this.speed = speed;
+
+
+
+    }
+
+
+
+
+
+
+
 }
--- src/main/java/com/group6/RobotRover/ProximitySensor.java
+++ src/main/java/com/group6/RobotRover/ProximitySensor.java
@@ -3,4 +3,32 @@ package com.group6.RobotRover;
 import java.util.Observable;
 
 public class ProximitySensor extends Observable {
+
+    private float distance;
+    private String sensorType;
+    ProximitySensor proximitySensor;
+
+    public ProximitySensor(int sensorType) {
+
+        switch (sensorType) {
+            case 1: proximitySensor = new UltraSonic();
+            break;
+            case 2: proximitySensor = new Laser();
+            break;
+            default: System.out.print("invalid choice for proximity sensor");
+            break;
+        }
+    }
+
+    public ProximitySensor() {
+
+    }
+
+
+    public float getDistance() {
+
+        return proximitySensor.getDistance();
+    }
+
+
 }
