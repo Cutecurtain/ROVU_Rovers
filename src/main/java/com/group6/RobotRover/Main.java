16a34924d0b4c4b6bcae8025fb1cffd6bba74d37
Aron Sandstedt <aron.sandstedt@live.se>
2018-12-10 15:25:15 +0100

    add planner feature

--- src/main/java/com/group6/RobotRover/Main.java
+++ src/main/java/com/group6/RobotRover/Main.java
@@ -1,9 +1,22 @@
 package com.group6.RobotRover;
 
-public class Main {
+import project.AbstractSimulatorMonitor;
+import simbad.sim.EnvironmentDescription;
 
-    public static void main (String[] strings) {
+import java.util.Set;
+
+public class Main extends AbstractSimulatorMonitor<Planner> {
+
+    public Main(Set<Planner> robots, EnvironmentDescription e) {
+        super(robots, e);
+    }
+
+    public void update(Planner planner) {
 
     }
+
+//    public static void main (String[] strings) {
+//
+//    }
 
 }
--- src/main/java/com/group6/RobotRover/Planner.java
+++ src/main/java/com/group6/RobotRover/Planner.java
@@ -3,12 +3,29 @@ package com.group6.RobotRover;
 import project.AbstractRobotSimulator;
 import project.Point;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Planner extends AbstractRobotSimulator {
+
+    private List<Point> missionPoints;
 
     public Planner(Point position, String name) {
         super(position, name);
+        this.missionPoints = new ArrayList<Point>();
     }
 
-    
+    public void addMissionPoint(Point... points) {
+        Collections.addAll(this.missionPoints, points);
+    }
+
+    public void followPath() {
+        for (Point point : missionPoints) {
+            super.setDestination(point);
+            super.getAgent().performBehavior();
+            while (!super.isAtPosition(point));
+        }
+    }
 
 }
