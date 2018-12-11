18609f1026a4e22e7071f2ca0bb7fec9041bff52
Aron Sandstedt <aron.sandstedt@live.se>
2018-12-11 13:46:14 +0100

    remove simulator monitor from the main on the client side

--- src/main/java/com/group6/RobotRover/Main.java
+++ src/main/java/com/group6/RobotRover/Main.java
@@ -1,19 +1,6 @@
 package com.group6.RobotRover;
 
-import project.AbstractSimulatorMonitor;
-import simbad.sim.EnvironmentDescription;
-
-import java.util.Set;
-
-public class Main extends AbstractSimulatorMonitor<Planner> {
-
-    public Main(Set<Planner> robots, EnvironmentDescription e) {
-        super(robots, e);
-    }
-
-    public void update(Planner planner) {
-
-    }
+public class Main {
 
 //    public static void main (String[] strings) {
 //
