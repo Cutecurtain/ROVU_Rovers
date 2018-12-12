e86a99060ff9e4fbc24bd6912d17f7555bc682bf
Aron Sandstedt <aron.sandstedt@live.se>
2018-12-12 11:15:44 +0100

    added networking stuff client-side

--- src/main/java/com/group6/RobotRover/Networking.java
+++ src/main/java/com/group6/RobotRover/Networking.java
@@ -1,4 +1,33 @@
 package com.group6.RobotRover;
 
 public class Networking {
+
+    private static final Networking SINGLETON = new Networking();
+
+    private com.group6.Server.Networking server; // Pretend network connection
+
+    private Networking() {
+        server = com.group6.Server.Networking.getInstance();
+    }
+
+    public static Networking getInstance() {
+        return SINGLETON;
+    }
+
+    public boolean connect(Planner planner) {
+        return server.request(com.group6.Server.Networking.Instruction.CONNECT, planner);
+    }
+
+    public boolean update(Planner planner) {
+        return server.request(com.group6.Server.Networking.Instruction.UPDATE, planner);
+    }
+
+    public boolean finishMission(Planner planner) {
+        return server.request(com.group6.Server.Networking.Instruction.FINISH_MISSION, planner);
+    }
+
+    public boolean fault(Planner planner) {
+        return server.request(com.group6.Server.Networking.Instruction.FAULT, planner);
+    }
+    
 }
