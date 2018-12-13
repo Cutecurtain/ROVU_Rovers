1f319d8cc8b80141264fc60e31dca617fb3471be
Aron Sandstedt <aron.sandstedt@live.se>
2018-12-13 15:27:15 +0100

    the room is now always a physical area

--- src/main/java/com/group6/Server/Environment/Area/Room.java
+++ src/main/java/com/group6/Server/Environment/Area/Room.java
@@ -3,11 +3,13 @@ package com.group6.Server.Environment.Area;
 import java.awt.geom.Point2D;
 
 public class Room extends Area implements INestable{
-    public Room(boolean isPhysical, int rewardPoints, Point2D position) {
-        super(isPhysical, rewardPoints, position);
+
+    public Room(int rewardPoints, Point2D position) {
+        super(true, rewardPoints, position);
     }
 
-    public Room(boolean isPhysical, int rewardPoints, Point2D position, IShape shape) {
-        super(isPhysical, rewardPoints, position, shape);
+    public Room(int rewardPoints, Point2D position, IShape shape) {
+        super(true, rewardPoints, position, shape);
     }
+
 }
