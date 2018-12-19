437ec9eb73cd50d22a7b6f6385339b0f12f7f9eb
AravindanAnbarasu <aravindan19675@gmail.com>
2018-12-19 12:05:40 +0100

    test case for strategy

--- src/test/java/com/group6/Server/Robot/RobotTest.java
+++ src/test/java/com/group6/Server/Robot/RobotTest.java
@@ -4,13 +4,24 @@ import com.group6.RobotRover.FaultHandler;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
 public class RobotTest {
+
 
     private Mission mission;
 
     private Robot robot = new Robot(null, null);
 
     private FaultHandler faultHandler;
+
+    // test case for checking strategy
 
     @Before
     public void setUp() {
@@ -29,6 +40,26 @@ public class RobotTest {
 
         //assertTrue(robot.completeMission());
     }
+
+    @Test
+
+    public void testStrategy(){
+        List<Point2D> missionPoints = new ArrayList<Point2D>();
+        List<Point2D> results ;
+
+        missionPoints.add(new Point2D.Double(1,1));
+        missionPoints.add(new Point2D.Double(2,2));
+        missionPoints.add(new Point2D.Double(1,3));
+        missionPoints.add(new Point2D.Double(2,6));
+
+        Strategy strategy = new Strategy(missionPoints);
+
+        results = strategy.nearestPath(missionPoints);
+
+        System.out.println(results);
+        System.out.println(missionPoints);
+    }
+
 
 
 
